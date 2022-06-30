937fff7413446baa509ece99987e3e3c051e7c78
TsaybelSA <tsaybelsa@gmail.com>
2022-06-30 12:06:35 +0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Сергей Цайбель on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
